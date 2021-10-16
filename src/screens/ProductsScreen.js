41e1f442af20bde4ceb8b62f5017d5f62b6aab08
SDushan <samithadushan@gmail.com>
2021-10-16 23:24:05 +0530

    added redux connect to product screen

--- src/screens/ProductsScreen.js
+++ src/screens/ProductsScreen.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Text, View } from 'react-native';
+import { connect } from 'react-redux';
+import { fetchProducts } from '../actions'
 
-const ProductsScreen = () => {
+const ProductsScreen = ({ onFetchProducts }) => {
+  useEffect(() => {
+    onFetchProducts();
+  }, [])
+
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>ProductsScreen</Text>
@@ -9,4 +15,16 @@ const ProductsScreen = () => {
   );
 }
 
-export default ProductsScreen;
+const mapStateToProps = (state) => ({
+  products: state.products
+})
+
+const mapDispatchToProps = (dispatch) => {
+  return ({
+    onFetchProducts: () => {
+      dispatch(fetchProducts());
+    }
+  })
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProductsScreen);
