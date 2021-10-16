92e60cfd96b261f696b3faae77fbff83a32ad298
SDushan <samithadushan@gmail.com>
2021-10-16 22:40:41 +0530

    added product screen

---
+++ src/screens/ProductsScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const ProductsScreen = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>ProductsScreen</Text>
+    </View>
+  );
+}
+
+export default ProductsScreen;
